8b61d4e4c6bef55deff5a5ca1d041cd5e3494ae9
Marcos Leroy <122321959+MarcosLeroy@users.noreply.github.com>
2023-08-09 14:48:42 -0300

    Adiciona componente de Loading

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../../images/logo.svg";
+import AppLoading from "../organisms/AppLoading";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -18,7 +19,7 @@ export default function Home() {
   }, []);
 
   return isLoading ? (
-    <h1>Loading...</h1>
+    <AppLoading />
   ) : (
     <div className="home center">
       <div className="home__logo">
